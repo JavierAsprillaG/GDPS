307f75e72f283b09ce94ccec06569f1d7103656c
LuisGalarcio <lasprilla@unal.edu.co>
2024-08-17 03:16:10 -0500

    archivos ignorados

--- Habbo_Juego.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="HabboJuego" tilewidth="48" tileheight="48" tilecount="100" columns="10">
- <image source="../../../Escritorio/Modern tiles_Free/Old/Tileset_48x48_9.png" width="480" height="480"/>
-</tileset>
